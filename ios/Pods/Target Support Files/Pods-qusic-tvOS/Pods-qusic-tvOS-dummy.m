2d3d67c5a957ab6ba0d9edf6754b9110896f5e13
Jonas Triki <triki@Trikis-MacBook-Pro.local>
2019-12-22 02:05:45 +0100

    Initial project setup

---
+++ ios/Pods/Target Support Files/Pods-qusic-tvOS/Pods-qusic-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qusic_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_qusic_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-qusicTests/Pods-qusicTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qusicTests : NSObject
+@end
+@implementation PodsDummy_Pods_qusicTests
+@end
